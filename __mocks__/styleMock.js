dd07c6bae7c48425e165e731e6ffdfc6e74b18a8
Simon Schwartz <simon.schwartz0@gmail.com>
2018-05-23 15:39:45 +1000

    add loader and NASA1975 components

---
+++ __mocks__/styleMock.js
@@ -0,0 +1,3 @@
+// this mocks imported css files in our react components
+// we need to do this for jest
+module.exports = {};
--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
+import NASA1975Story from "../lib/components/NASA1975/NASA1975.story.js";
 import WormLogoStory from "../lib/components/WormLogo/WormLogo.story.js";
+import LoaderStory from "../lib/components/Loader/Loader.story.js";
 
 const componentStories = storiesOf("Components", module);
 componentStories.addDecorator(withKnobs);
 
 componentStories.add("WormLogo", () => <WormLogoStory />);
+componentStories.add("NASA1975", () => <NASA1975Story />);
+componentStories.add("Loader", () => <LoaderStory />);
