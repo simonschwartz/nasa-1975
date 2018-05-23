7cae88f9a88973980cded253c237578b5cd488c5
Simon Schwartz <simon.schwartz0@gmail.com>
2018-05-23 20:02:11 +1000

    add Heading component

--- stories/index.js
+++ stories/index.js
@@ -4,6 +4,7 @@ import { withKnobs } from "@storybook/addon-knobs";
 import NASA1975Story from "../lib/components/NASA1975/NASA1975.story.js";
 import WormLogoStory from "../lib/components/WormLogo/WormLogo.story.js";
 import LoaderStory from "../lib/components/Loader/Loader.story.js";
+import HeadingStory from "../lib/components/Heading/Heading.story.js";
 
 const componentStories = storiesOf("Components", module);
 componentStories.addDecorator(withKnobs);
@@ -11,3 +12,4 @@ componentStories.addDecorator(withKnobs);
 componentStories.add("WormLogo", () => <WormLogoStory />);
 componentStories.add("NASA1975", () => <NASA1975Story />);
 componentStories.add("Loader", () => <LoaderStory />);
+componentStories.add("Heading", () => <HeadingStory />);
