0af2cac29fe8504ef6635ed7ff4c20959d596fff
Simon Schwartz <simon.schwartz0@gmail.com>
2018-05-23 12:13:13 +1000

    update storybook doc strucutre with addon-info

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,20 @@
 import { configure } from "@storybook/react";
+import { setDefaults } from "@storybook/addon-info";
+
+setDefaults({
+  header: false,
+  inline: true,
+  styles: {
+    infoBody: {
+      border: "none",
+      padding: "0px",
+      borderTop: "1px solid rgb(238, 238, 238)",
+      paddingTop: "20px",
+      marginTop: "25px",
+      boxShadow: "none"
+    }
+  }
+});
 
 function loadStories() {
   require("../stories");
