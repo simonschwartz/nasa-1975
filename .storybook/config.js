3d38ed81c898e200db0567337a0fb95ebadf56ff
Simon Schwartz <simon.schwartz0@gmail.com>
2019-09-12 13:36:18 +1000

    Update tests

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,25 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+import { create } from '@storybook/theming';
+import { FONT_FAMILY_DEFAULT } from '../lib/tokens';
+
+const theme = create({
+    base: 'light',
+
+    // Typography
+    fontBase: FONT_FAMILY_DEFAULT,
+
+    brandTitle: 'NASA 1975',
+    brandImage: 'https://github.com/simonschwartz/nasa-1975/raw/master/nasa-worm.png',
+});
+
+addParameters({
+    options: {
+        theme,
+        storySort: (a, b) => {
+            if (a[1].id === 'welcome--page') return -1;
+            return a[1].id.localeCompare(b[1].id);
+        },
+    },
+});
 
 configure(require.context('../lib/', true, /\.stories\.(js|mdx)$/), module);
