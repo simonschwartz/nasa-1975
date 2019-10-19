ea37ca110f5cb2f54ce6e731ad570916384f867a
Simon Schwartz <simon.schwartz0@gmail.com>
2019-10-20 08:24:50 +1100

    Tidy up WormLogo docs and remove Typography as an export

---
+++ .storybook/components/StoryContainer/StoryContainer.js
@@ -0,0 +1,53 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
+import PropTypes from 'prop-types';
+import { NASA_RED, NASA_DARK, NASA_LIGHT, NASA_GREY } from '../../../lib/tokens';
+
+const DEFAULT_BACKGROUND = 'light';
+
+const BACKGROUND_COLORS = {
+    red: {
+        backgroundColor: NASA_RED,
+    },
+    dark: {
+        backgroundColor: NASA_DARK,
+    },
+    light: {
+        backgroundColor: NASA_LIGHT,
+    },
+    grey: {
+        backgroundColor: NASA_GREY,
+    },
+    medium: {
+        backgroundColor: '#F2F2F2',
+    },
+};
+
+const containerStyle = backgroundColor => {
+    const borderColor = backgroundColor === NASA_LIGHT ? '#eee' : backgroundColor;
+    return css`
+        display: block;
+        border-radius: 14px;
+        padding: 5rem;
+        margin: 2rem 0;
+        background: ${backgroundColor};
+        max-width: 20rem;
+        border: 1px solid ${borderColor};
+    `;
+};
+
+const StoryContainer = ({ background = DEFAULT_BACKGROUND, children }) => {
+    const { backgroundColor } =
+        BACKGROUND_COLORS[background] || BACKGROUND_COLORS[DEFAULT_BACKGROUND];
+
+    return <div css={containerStyle(backgroundColor)}>{children}</div>;
+};
+
+StoryContainer.propTypes = {
+    /** Background colour of container */
+    background: PropTypes.oneOf(['dark', 'light', 'red', 'grey']),
+    /** Container content */
+    children: PropTypes.node.isRequired,
+};
+
+export default StoryContainer;
--- .storybook/config.js
+++ .storybook/config.js
@@ -3,20 +3,21 @@ import { create } from '@storybook/theming';
 import { FONT_FAMILY_DEFAULT } from '../lib/tokens';
 
 const theme = create({
-    base: 'light',
-    fontBase: FONT_FAMILY_DEFAULT,
-    brandTitle: 'NASA 1975',
-    brandImage: 'https://github.com/simonschwartz/nasa-1975/raw/master/nasa-worm.png',
+  base: 'light',
+  fontBase: FONT_FAMILY_DEFAULT,
+  fontSize: '18px',
+  brandTitle: 'NASA 1975',
+  brandImage: 'https://github.com/simonschwartz/nasa-1975/raw/master/nasa-worm.png',
 });
 
 addParameters({
-    options: {
-        theme,
-        storySort: (a, b) => {
-            if (a[1].id === 'welcome--page') return -1;
-            return a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id);
-        },
+  options: {
+    theme,
+    storySort: (a, b) => {
+      if (a[1].id === 'welcome--page') return -1;
+      return a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id);
     },
+  },
 });
 
 configure(require.context('../lib/', true, /\.stories\.(js|mdx)$/), module);
